7dcb6536bff0421fa3d25d8e0a1ab26a700d8837
Gurpreet Heir <heirgurpreet@gmail.com>
2019-02-24 18:32:08 -0800

    vuex added

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create a Event, {{user.name}}</h1>
+    <p>I am {{user.id}}</p>
+    <p>There are {{catLength}} categores</p>
+    <ul>
+      <li v-for="(cat, index) in categories" :key="cat"> {{cat}} </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
